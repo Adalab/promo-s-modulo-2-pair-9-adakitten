cf2b5593bb5cf3bf6a432965c369326a543fadd2
Natalia Blanco <nataliaouteiral@gmail.com>
2023-02-06 11:25:18 +0100

    added event form

--- js/main.js
+++ js/main.js
@@ -3,61 +3,53 @@
 section.classList.remove('collapsed'); */
 
 const kittenlist = document.querySelector('.js-list');
+const kittenImage1 = 'https://dev.adalab.es/gato-siames.webp';
+const kittenName1 = 'Anastacio';
+const kittenDesc1 =
+  'porte elegante, su patron de color tan caracterisitico y sus ojos de un azul intenso';
+const kittenRace1 = 'siamés';
 
+const kittenone = `<li class="card">
+          <img class="card_img" src=${kittenImage1} alt="maine-coon-cat" />
+          <h3 class="card_title">${kittenName1}</h3>
+          <h4 class="card_race">Maine Coon</h4>
+          <p class="card_description">${kittenDesc1}</p>
+        </li>`;
+const kittenImage2 = 'https://dev.adalab.es/sphynx-gato.webp';
+const kittenName2 = 'Fiona';
+const kittenDesc2 =
+  'Produce fascinacion y curiosidad. Exotico, raro, bello, extraño...hasta con pinta de alienigena han llegadoa  definir a esta razagatuna';
 
-
-
-
+const kittenRace2 = 'Sphynx';
 const kittentwo = `<li class="card">
-         <img class="card_img" src="https://dev.adalab.es/sphynx-gato.webp" alt="sphynx-cat" />
-          <h3 class="card_title">Fiona</h3>
-          <h4 class="card_race">Sphynx</h4>
+         <img class="card_img" src=${kittenImage2} alt="sphynx-cat" />
+          <h3 class="card_title">${kittenName2}</h3>
+          <h4 class="card_race">${kittenRace2}</h4>
           <p class="card_description">
-            Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-            hasta con pinta de alienígena han llegado a definir a esta raza
-            gatuna que se caracteriza por la «ausencia» de pelo.
+            ${kittenDesc2}
           </p>
         </li>`;
 
+const kittenImage3 = 'https://dev.adalab.es/maine-coon-cat.webp';
+const kittenName3 = 'Cielo';
+const kittenDesc3 =
+  'Tiene la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en unas de sus señas de identidad ';
+const kittenRace3 = 'Maine coon';
 const kittenthree = `<li class="card">
-          <img class="card_img" src="https://dev.adalab.es/maine-coon-cat.webp" alt="maine-coon-cat" />
-          <h3 class="card_title">Cielo</h3>
-          <h4 class="card_race">Maine Coon</h4>
-          <p class="card_description">
-            Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-            bella mirada se ha convertido en una de sus señas de identidad.
-            Sus ojos son grandes y las orejas resultan largas y en punta.
+          <img class="card_img" src=${kittenImage3} alt="maine-coon-cat" />
+          <h3 class="card_title">${kittenName3}</h3>
+          <h4 class="card_race">${kittenRace3}</h4>
+          <p class="card_description"> ${kittenDesc3}
           </p>
         </li>`;
 kittenlist.innerHTML = `${kittenone} ${kittentwo} ${kittenthree}`;
 
-
-const kittenImage1 = '<img src= "https://dev.adalab.es/gato-siames.webp" />';
-
-const kittenName1='Anastacio';
-
-const kittenDesc1= 'porte elegante, su patron de color tan caracterisitico y sus ojos de un azul intenso';
-
-const kittenRace1 = 'siamés';
-
-const kittenImage2 = '<img src="https://dev.adalab.es/sphynx-gato.webp" />';
-
-const kittenName2='Fiona';
-
-const kittenDesc2= 'Produce fascinacion y curiosidad. Exotico, raro, bello, extraño...hasta con pinta de alienigena han llegadoa  definir a esta razagatuna';
-
-const kittenRace2 = 'Sphynx';
-
-const kittenImage3 = 'https://dev.adalab.es/maine-coon-cat.webp"/>';
-
-const kittenName3='Cielo';
-
-const kittenDesc3= 'Tiene la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en unas de sus señas de identidad ';
-
-const kittenRace3 = 'Maine coon';
-
-const kittenOne.innerHTML = `${kittenImage1} ${kittenName1} ${kittenDesc1} ${kittenRace1}`;
-
-
-
-
+const buttonplus = document.querySelector('.buttonplus');
+const formElement = document.querySelector('.js-new-form');
+buttonplus.addEventListener('click', () => {
+  if (formElement.classList.contains('collapsed')) {
+    formElement.classList.remove('collapsed');
+  } else {
+    formElement.classList.add('collapsed');
+  }
+});
