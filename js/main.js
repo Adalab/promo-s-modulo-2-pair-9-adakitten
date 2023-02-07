6336fe8b2a6e8f4206dc8e4655e81a9aae8d5bde
Virlucero <virlucero@gmail.com>
2023-02-07 10:23:02 +0000

    add event

--- js/main.js
+++ js/main.js
@@ -53,3 +53,27 @@ buttonplus.addEventListener('click', () => {
     formElement.classList.add('collapsed');
   }
 });
+
+const inputDesc = document.querySelector('.js-input-desc');
+const inputPhoto = document.querySelector('.js-input-photo');
+const inputName = document.querySelector('.js-input-name');
+const labelMessageError = document.querySelector('.js-label-error');
+const newButton= document.querySelector ('.js-btn-add')
+newButton.addEventListener('click',(event)=> {
+  event.preventDefault();
+
+const valueDesc = inputDesc.value;
+const valuePhoto = inputPhoto.value;
+const valueName = inputName.value;
+  
+  
+if (valueDesc === '' || valuePhoto === '' || valueName === '') {
+labelMessageError.innerHTML='uy!parece que has olvidado algo';
+}
+});
+
+
+
+
+
+
